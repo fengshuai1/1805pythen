4cf1d1afcb1a652de57bc31842eac39df69d5fd4
fengshuai1 <1329008013@qq.com>
2018-06-05 16:42:48 +0800

    单词风暴

---
+++ 3.py
@@ -0,0 +1,22 @@
+1：单词风暴
+Download:下载
+Install:安装
+Install Now:立刻安装
+Customize Installation: 自定义安装
+Browse:浏览（一般用于选择安装目录）
+UnInstall:卸载
+Cancel:取消
+Next:下一步
+Back:返回
+Finish:结束
+Close:关闭
+Open:关闭
+System:系统
+BIOS:基本输出系统
+Virtualization:虚拟化
+Default:默认
+Path:路径
+Account: 账号
+Password:密码
+NickName:昵称
+
